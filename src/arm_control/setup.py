f54730eae104779ae5950884848695060d04fbf6
conruybalid <conruybalid@gmail.com>
2024-07-10 22:31:53 +0000

    Got rid of a node that wasn't actually there

--- src/arm_control/setup.py
+++ src/arm_control/setup.py
@@ -20,7 +20,6 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'arm_control = arm_control.arm_move_node:main',
             'arm_move_action = arm_control.arm_move_action:main',
         ],
     },
