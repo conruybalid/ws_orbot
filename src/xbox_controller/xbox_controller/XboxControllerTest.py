77d7116d9bdaad7c784910028ee35ac915e6eae5
conruybalid <conruybalid@gmail.com>
2024-08-01 17:16:53 +0000

    Fixed controller disconnect issue

--- src/xbox_controller/xbox_controller/XboxControllerTest.py
+++ src/xbox_controller/xbox_controller/XboxControllerTest.py
@@ -1,11 +1,20 @@
-from inputs import get_gamepad
+import time
+import importlib
+import inputs
 
 def main():
     print("Listening for gamepad events...")
     while True:
-        events = get_gamepad()
-        for event in events:
-            print(event.ev_type, event.code, event.state)
+        try:
+            events = inputs.get_gamepad()
+            for event in events:
+                print(event.ev_type, event.code, event.state)
+        except Exception as e:
+            print("Error:", str(e))
+            time.sleep(5)
+            importlib.reload(inputs)  # Re-import the devices module to reinitialize
+
+            continue
 
 if __name__ == "__main__":
     main()
--- src/xbox_controller/xbox_controller/controller_node.py
+++ src/xbox_controller/xbox_controller/controller_node.py
@@ -1,9 +1,18 @@
 import rclpy
 from rclpy.node import Node
-from custom_interfaces.msg import Tank
-import time
+from rclpy.action import ActionClient
 
-from inputs import get_gamepad
+from custom_interfaces.msg import Tank
+
+from custom_interfaces.msg import ArmControl
+from custom_interfaces.action import MoveArm
+
+import time
+from typing import List
+
+import inputs
+import importlib
+
 
 def map_value(value, in_min, in_max, out_min, out_max):
     return int((value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
@@ -32,20 +41,50 @@ class XboxPublisher(Node):
         self.left_speed = 0
         self.right_speed = 0
 
+        self.arm_action_client = ActionClient(self, MoveArm, 'move_arm_action')
+
+        self.waypoints: List[MoveArm.Goal] = []
+
+
         self.get_logger().info("Xbox Publisher Node Initialized")
 
     def check_controller(self):
         try:
-            events = get_gamepad()
-        except:
-            self.get_logger().warn("No controller found")
-            time.sleep(15)
+            events = inputs.get_gamepad()
+        except Exception as e:
+            self.get_logger().warn(str(e))
+            time.sleep(5)
+            importlib.reload(inputs) # Re-import the devices module to reinitialize
             return
         for event in events:
             if event.ev_type == 'Key':
                 if event.code == 'BTN_SOUTH' and event.state == 1:
                     self.manual_control = not self.manual_control
-                    self.get_logger().info(f"Manual Control: {self.manual_control}")  
+                    self.get_logger().info(f"Manual Control: {self.manual_control}")
+                
+                if not self.manual_control:
+                    if event.code == 'BTN_NORTH' and event.state == 1:
+                        self.get_logger().info("Moving Arm Left")
+                        goal_msg = self.format_move_goal([0.4,0.5,0.3], [0.0,135.0,90.0], 0, 3) # 3 is the base reference frame
+                        self.waypoints.append(goal_msg)
+                        goal_msg = self.format_move_goal([0.5,0.5,0.0], [0.0,180.0,90.0], 0, 3) # 3 is the base reference frame
+                        self.waypoints.append(goal_msg)
+                        return #So that only command is sent
+                    if event.code == 'BTN_EAST' and event.state == 1:
+                        self.get_logger().info("Moving Arm Right")
+                        goal_msg = self.format_move_goal([-0.4,0.5,0.3], [0.0,45.0,90.0], 0, 3) # 3 is the base reference frame
+                        self.waypoints.append(goal_msg)
+                        goal_msg = self.format_move_goal([-0.5,0.5,0.0], [0.0,0.0,90.0], 0, 3)
+                        self.waypoints.append(goal_msg)
+                        return
+                    if event.code == 'BTN_WEST' and event.state == 1:
+                        self.get_logger().info("Moving Arm forward")
+                        goal_msg = self.format_move_goal([0.0,0.5,0.6], [0.0,90.0,90.0], 0, 3)
+                        self.waypoints.append(goal_msg)
+                        goal_msg = self.format_move_goal([0.0,0.5,0.5], [0.0,90.0,90.0], 0, 3)
+                        self.waypoints.append(goal_msg)
+                        return
+
             if event.ev_type == 'Absolute':
                 if event.code == 'ABS_Y':
                     self.left_speed = map_value(event.state, -32768, 32767, 1000, -1000)
@@ -54,8 +93,51 @@ class XboxPublisher(Node):
                     self.right_speed = map_value(event.state, -32768, 32767, 1000, -1000)
 
         return
+
+    def format_move_goal(self, position=[0.0,0.0,0.0], angle=[0.0,0.0,0.0], gripper_state=0, reference_frame=2): # 2 is the arm reference frame
+        """
+        Quickly formates a goal that can be sent to the arm action server.
+        """
+        goal_msg = MoveArm.Goal()
+        goal_msg.goal.position.x = position[0]
+        goal_msg.goal.position.y = position[1]
+        goal_msg.goal.position.z = position[2]
+        goal_msg.goal.angle.x = angle[0]
+        goal_msg.goal.angle.y = angle[1]
+        goal_msg.goal.angle.z = angle[2]
+        goal_msg.goal.reference_frame = reference_frame
+        goal_msg.goal.gripper_state = gripper_state
+
+        return goal_msg
     
-    def publish(self, L, R):
+    def send_move_goal(self, goal_msg):
+        """
+        Sends an waypoint goal to the arm action server.
+        Waits for the server to respond and returns the result.
+        """
+
+        self.arm_action_client.wait_for_server()
+        future = self.arm_action_client.send_goal_async(goal_msg)
+        #future.add_done_callback(self.goal_response_callback)
+        rclpy.spin_until_future_complete(self, future)
+
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().error('Move Goal rejected :(')
+            return False
+
+        self.get_logger().debug(f'Move Goal sent: \n{goal_msg}')
+
+        result_future = goal_handle.get_result_async()
+        #result_future.add_done_callback(self.get_result_callback)
+        rclpy.spin_until_future_complete(self, result_future)
+        result = result_future.result().result
+        self.get_logger().debug(f'Move Result: {result.result}')
+
+        
+        return result.result    
+    
+    def publish_tank(self, L, R):
         if L < 100 and L > -100:
             L = 0
         if R < 100 and R > -100:
@@ -74,7 +156,12 @@ def main(args=None):
         while rclpy.ok():
             node.check_controller()
             if node.manual_control:
-                node.publish(node.left_speed, node.right_speed)
+                node.publish_tank(node.left_speed, node.right_speed)
+
+            else:
+                for waypoint in node.waypoints:
+                    node.send_move_goal(waypoint)
+                node.waypoints = []
             #rclpy.spin_once(node)
 
     except KeyboardInterrupt:
