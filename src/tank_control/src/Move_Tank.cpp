007b26435f04e3f511243ce6e4fc2658fec908af
conruybalid <conruybalid@gmail.com>
2024-08-01 00:26:06 +0000

    This should try to connect to both ACM0 and ACM1 now

--- src/tank_control/src/Move_Tank.cpp
+++ src/tank_control/src/Move_Tank.cpp
@@ -41,6 +41,8 @@ class MoveTank : public rclcpp::Node
     MoveTank(string input_port)
     : Node("move_tank"), device_()
     {
+      RCLCPP_INFO(this->get_logger(), "Initailizing MoveTank Node");
+
       subscriber = this->create_subscription<custom_interfaces::msg::Tank>(
       "move_tank_commands", 10, std::bind(&MoveTank::command_callback, this, std::placeholders::_1));
 
@@ -49,7 +51,14 @@ class MoveTank : public rclcpp::Node
 
         if (status != RQ_SUCCESS)
         {
-            RCLCPP_ERROR(this->get_logger(), "Unable to connect to device, status: %d", status);
+            RCLCPP_ERROR(this->get_logger(), "Unable to connect to device, status: %d\n attempting to connect ACM1", status);
+            port = "/dev/ttyACM1";
+            status = device_.Connect(port);
+            if (status != RQ_SUCCESS)
+            {
+                RCLCPP_ERROR(this->get_logger(), "Still unable to connect to device, status: %d\n", status);
+            }
+
         }
 
         // Set to digital input
@@ -144,7 +153,7 @@ class MoveTank : public rclcpp::Node
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MoveTank>("/dev/ttyACM1"));
+  rclcpp::spin(std::make_shared<MoveTank>("/dev/ttyACM0"));
   rclcpp::shutdown();
   return 0;
 }
